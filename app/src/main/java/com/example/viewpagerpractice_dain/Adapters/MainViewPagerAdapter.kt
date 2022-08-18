77ef32d2c1f1de27d11169ca43653ed919e38419
DI <cdi122@nate.com>
2022-08-18 16:59:07 +0900

    코틀린 조건문인 When 활용 예시

--- app/src/main/java/com/example/viewpagerpractice_dain/Adapters/MainViewPagerAdapter.kt
+++ app/src/main/java/com/example/viewpagerpractice_dain/Adapters/MainViewPagerAdapter.kt
@@ -10,28 +10,20 @@ import com.example.viewpagerpractice_dain.fragments.NameFragment
 class MainViewPagerAdapter(fm : FragmentManager) : FragmentPagerAdapter(fm) {
 
     override fun getPageTitle(position: Int): CharSequence? {
-        if (position == 0){
-            return "이름"
-        }
-        else if (position == 1){
-            return "정보"
-        }
-        else {
-            return "인사말"
+        return when (position) {
+            0 -> "이름"
+            1 -> "내 정보"
+            else -> "인사말"
         }
 
     }
 
     override fun getItem(position: Int): Fragment {
-        if (position == 0) {
-            return NameFragment()
-        }
-        else if (position == 1) {
-            return MyinfoFragment()
-        }
-        else {
 
-        return GreetingsFragment()
+        return when(position) {
+            0 -> NameFragment()
+            1 -> MyinfoFragment()
+            else -> GreetingsFragment()
         }
     }
     override fun getCount(): Int {
