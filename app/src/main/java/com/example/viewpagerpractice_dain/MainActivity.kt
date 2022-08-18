aa973f6566d1cdff0f8af5d3f82e41b675e6aa1a
DI <cdi122@nate.com>
2022-08-18 15:53:54 +0900

    추가적 페이지 생성시 대응 코드 추가

--- app/src/main/java/com/example/viewpagerpractice_dain/MainActivity.kt
+++ app/src/main/java/com/example/viewpagerpractice_dain/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.viewpagerpractice_dain
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.viewpager.widget.PagerAdapter
+import com.example.viewpagerpractice_dain.Adapters.MainViewPagerAdapter
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var mViewPagerAdapter: MainViewPagerAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mViewPagerAdapter = MainViewPagerAdapter(supportFragmentManager)
+        mainViewPager.adapter = mViewPagerAdapter
+        mainViewPager.offscreenPageLimit = 5
     }
 }
